88b5ad08f5c36928587a7fadab88339c50d20048
pztrn <pztrn@pztrn.ru>
2012-09-09 12:12:39 +0400

    Missing options dialog after refactoring

---
+++ src/lib/options_dialog.py
@@ -0,0 +1,112 @@
+# -*- coding: utf8 -*-
+
+import os
+from PyQt4.QtCore import QString
+from PyQt4.QtGui import QDialog
+from ui import Settings
+from lib import common
+
+class Options_Dialog(QDialog):
+    def __init__(self, settings, callback, parent = None):
+        QDialog.__init__(self, parent)
+        self.ui = Settings.Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.settings = settings
+        self.callback = callback
+        self.ui.accountName.setText(self.settings["user"])
+        self.ui.serverName.setText(self.settings["server"])
+        self.ui.password.setText(self.settings["password"])
+        if self.settings["useSecureConnection"] == "1":
+            self.ui.useSecureConnectionCheckbox.setCheckState(2)
+        else:
+            self.ui.useSecureConnectionCheckbox.setCheckState(0)
+        if self.settings["isSingle"] == "1":
+            self.ui.suiCheckbox.setCheckState(2)
+            self.ui.mllabel.setEnabled(True)
+            self.ui.messageLength.setEnabled(True)
+        else:
+            self.ui.suiCheckbox.setCheckState(0)
+            self.ui.mllabel.setEnabled(False)
+            self.ui.messageLength.setEnabled(False)
+        self.ui.messageLength.setText(self.settings["messageLength"])
+        if self.settings["deleteAllFromCacheOnExit"] == "1":
+            self.ui.removePostsOnExitCheckbox.setCheckState(2)
+        else:
+            self.ui.removePostsOnExitCheckbox.setCheckState(0)
+        try:
+            self.ui.updateInterval.setValue(int(self.settings["updateInterval"]))
+        except:
+            self.ui.updateInterval.setValue(10)
+            
+        if self.settings["remember_last_dent_id"] == "1":
+            self.ui.remember_last_dentid.setCheckState(2)
+            self.ui.dents_quantity.setEnabled(False)
+        else:
+            self.ui.remember_last_dentid.setCheckState(0)
+            self.ui.dents_quantity.setEnabled(True)
+            
+        self.ui.dents_quantity.setText(self.settings["fetch_on_startup"])
+
+        self.ui.okButton.clicked.connect(self.transmitSettings)
+        self.ui.cancelButton.clicked.connect(self.close)
+        
+        self.ui.suiCheckbox.stateChanged.connect(self.changing_sui_state)
+        self.ui.remember_last_dentid.stateChanged.connect(self.changing_rld_state)
+        
+        # Now Playing
+        for item in common.GLOBAL_PARMS["players"]:
+            self.ui.players_list.addItem(item)
+        
+        try:
+            index = common.GLOBAL_PARMS["players"].index(self.settings["player"])
+            self.ui.players_list.setCurrentIndex(index)
+            self.ui.player_string.appendPlainText(self.settings["player_string"])
+        except:
+            # No config values, passing
+            pass
+        
+    def changing_sui_state(self):
+        if self.ui.suiCheckbox.checkState() == 0:
+            self.ui.messageLength.setEnabled(False)
+        else:
+            self.ui.messageLength.setEnabled(True)
+            
+    def changing_rld_state(self):
+        if self.ui.remember_last_dentid.checkState() == 2:
+            self.ui.dents_quantity.setEnabled(False)
+        else:
+            self.ui.dents_quantity.setEnabled(True)       
+
+    def transmitSettings(self):
+        settingslist = []
+        settingslist.append(str(self.ui.accountName.text()))
+        settingslist.append(str(self.ui.password.text()))
+        settingslist.append(str(self.ui.serverName.text()))
+        if self.ui.useSecureConnectionCheckbox.isChecked():
+            settingslist.append(str(1))
+        else:
+            settingslist.append(str(0))
+        if self.ui.suiCheckbox.isChecked():
+            settingslist.append(str(1))
+        else:
+            settingslist.append(str(0))
+        if self.ui.messageLength.text():
+            settingslist.append(str(self.ui.messageLength.text()))
+        else:
+            settingslist.append(str(140))
+        if self.ui.removePostsOnExitCheckbox.isChecked():
+            settingslist.append(str(1))
+        else:
+            settingslist.append(str(0))
+        settingslist.append(self.ui.updateInterval.value())
+        if self.ui.remember_last_dentid.isChecked():
+            settingslist.append(str(1))
+        else:
+            settingslist.append(str(0))
+        settingslist.append(str(self.ui.dents_quantity.text()))
+        settingslist.append(str(self.ui.players_list.currentText()))
+        settingslist.append(str(self.ui.player_string.toPlainText()))
+
+        self.callback(settingslist)
+        self.close()
